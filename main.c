d835a584ffbe33375b0d05520e0685b0d83af353
Alexander Kojevnikov <alexander@kojevnikov.com>
2011-09-18 13:41:26 +0800

    Formatting

--- main.c
+++ main.c
@@ -6,44 +6,46 @@
  * See LICENSE for licensing information
  */
 
-#include <string.h>
-#include <panel-applet.h>
-#include <gtk/gtklabel.h>
-#include <dbus/dbus-glib.h>
 #include <stdlib.h>
+#include <string.h>
+
+#include <gtk/gtk.h>
+#include <panel-applet.h>
+#include <dbus/dbus-glib.h>
 
 /* AW: I pulled this code off the interwebs. It makes background 
  * transparency actually work
  */
-static void change_bg(PanelApplet *applet, PanelAppletBackgroundType type,
-                      GdkColor *color, GdkPixmap *pixmap)
+static void change_bg(
+    PanelApplet *applet, PanelAppletBackgroundType type,
+    GdkColor *color, GdkPixmap *pixmap)
 {
-  GtkRcStyle *rc_style;
-  GtkStyle *style;
+    GtkRcStyle *rc_style;
+    GtkStyle *style;
 
-  /* reset style */
-  gtk_widget_set_style(GTK_WIDGET(applet), NULL);
-  rc_style = gtk_rc_style_new();
-  gtk_widget_modify_style(GTK_WIDGET(applet), rc_style);
-  gtk_rc_style_unref(rc_style);
+    /* reset style */
+    gtk_widget_set_style(GTK_WIDGET(applet), NULL);
+    rc_style = gtk_rc_style_new();
+    gtk_widget_modify_style(GTK_WIDGET(applet), rc_style);
+    gtk_rc_style_unref(rc_style);
 
-  switch(type) {
+    switch(type) {
     case PANEL_NO_BACKGROUND:
-      break;
+        break;
     case PANEL_COLOR_BACKGROUND:
-      gtk_widget_modify_bg(GTK_WIDGET(applet), GTK_STATE_NORMAL, color);
-      break;
+        gtk_widget_modify_bg(GTK_WIDGET(applet), GTK_STATE_NORMAL, color);
+        break;
     case PANEL_PIXMAP_BACKGROUND:
-      style = gtk_style_copy(GTK_WIDGET(applet)->style);
+        style = gtk_style_copy(GTK_WIDGET(applet)->style);
 
-      if(style->bg_pixmap[GTK_STATE_NORMAL])
-        g_object_unref(style->bg_pixmap[GTK_STATE_NORMAL]);
+        if(style->bg_pixmap[GTK_STATE_NORMAL])
+            g_object_unref(style->bg_pixmap[GTK_STATE_NORMAL]);
 
-      style->bg_pixmap[GTK_STATE_NORMAL] = g_object_ref(pixmap);
-      gtk_widget_set_style(GTK_WIDGET(applet), style);
-      g_object_unref(style);
-      break;
-  }
+        style->bg_pixmap[GTK_STATE_NORMAL] = g_object_ref(pixmap);
+        gtk_widget_set_style(GTK_WIDGET(applet), style);
+        g_object_unref(style);
+        break;
+    }
 }
 
 static void signal_handler(DBusGProxy *obj, const char *msg, GtkWidget *widget)
@@ -53,61 +55,60 @@ static void signal_handler(DBusGProxy *obj, const char *msg, GtkWidget *widget)
 
 static void set_up_dbus_transfer(GtkWidget *buf)
 {
-  DBusGConnection *connection;
-  DBusGProxy *proxy;
-  GError *error= NULL;
+    DBusGConnection *connection;
+    DBusGProxy *proxy;
+    GError *error= NULL;
 
-  connection = dbus_g_bus_get(DBUS_BUS_SESSION, &error);
-  if(connection == NULL) {
-    g_printerr("Failed to open connection: %s\n", error->message);
-    g_error_free(error);
-    exit(1);
-  }
+    connection = dbus_g_bus_get(DBUS_BUS_SESSION, &error);
+    if(connection == NULL) {
+        g_printerr("Failed to open connection: %s\n", error->message);
+        g_error_free(error);
+        exit(1);
+    }
 
-  proxy = dbus_g_proxy_new_for_name(connection, "org.xmonad.Log",
-                                   "/org/xmonad/Log",
-                                   "org.xmonad.Log");
-  error = NULL;
+    proxy = dbus_g_proxy_new_for_name(
+        connection, "org.xmonad.Log", "/org/xmonad/Log", "org.xmonad.Log");
+    error = NULL;
 
-  dbus_g_proxy_add_signal(proxy, "Update", G_TYPE_STRING, G_TYPE_INVALID);
-  dbus_g_proxy_connect_signal(proxy, "Update", (GCallback)signal_handler,
-                              buf, NULL);
+    dbus_g_proxy_add_signal(proxy, "Update", G_TYPE_STRING, G_TYPE_INVALID);
+    dbus_g_proxy_connect_signal(
+        proxy, "Update", (GCallback)signal_handler, buf, NULL);
 }
 
 static gboolean xmonadlog_applet_fill(PanelApplet *applet)
 {
-  GtkWidget *label = gtk_label_new("Waiting for Xmonad...");
+    GtkWidget *label = gtk_label_new("Waiting for Xmonad...");
 
-  gtk_label_set_use_markup(GTK_LABEL(label), TRUE);
-  gtk_misc_set_alignment((GtkMisc *)label, 0.0, 0.5);
-  set_up_dbus_transfer(label);
+    gtk_label_set_use_markup(GTK_LABEL(label), TRUE);
+    gtk_misc_set_alignment((GtkMisc *)label, 0.0, 0.5);
+    set_up_dbus_transfer(label);
 
-  g_signal_connect(applet, "change-background", G_CALLBACK(change_bg), NULL);
-  gtk_container_add(GTK_CONTAINER(applet), label);
-  gtk_widget_show_all(GTK_WIDGET(applet));
+    g_signal_connect(applet, "change-background", G_CALLBACK(change_bg), NULL);
+    gtk_container_add(GTK_CONTAINER(applet), label);
+    gtk_widget_show_all(GTK_WIDGET(applet));
 
-  return TRUE;
+    return TRUE;
 }
 
-static gboolean xmonadlog_applet_factory(PanelApplet *applet,
-                                         const gchar *iid,
-                                         gpointer data)
+static gboolean xmonadlog_applet_factory(
+    PanelApplet *applet, const gchar *iid, gpointer data)
 {
-  gboolean retval = FALSE;
+    gboolean retval = FALSE;
 
-  if(!strcmp(iid, "XmonadLogApplet"))
-    retval = xmonadlog_applet_fill(applet);
+    if(!strcmp(iid, "XmonadLogApplet"))
+        retval = xmonadlog_applet_fill(applet);
 
-  if(retval == FALSE) {
-    printf("Wrong applet!\n");
-    exit(-1);
-  }
+    if(retval == FALSE) {
+        printf("Wrong applet!\n");
+        exit(-1);
+    }
 
-  return retval;
+    return retval;
 }
 
-PANEL_APPLET_OUT_PROCESS_FACTORY("XmonadLogAppletFactory",
-                                 PANEL_TYPE_APPLET,
-                                 "XmonadLogApplet",
-                                 xmonadlog_applet_factory,
-                                 NULL);
+PANEL_APPLET_OUT_PROCESS_FACTORY(
+    "XmonadLogAppletFactory",
+    PANEL_TYPE_APPLET,
+    "XmonadLogApplet",
+    xmonadlog_applet_factory,
+    NULL);
