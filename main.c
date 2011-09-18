41ae59d02bd8dba56d35c63cd8f571778fda6aa5
Alexander Kojevnikov <alexander@kojevnikov.com>
2011-09-18 14:24:28 +0800

    Properly set the background

--- main.c
+++ main.c
@@ -13,41 +13,6 @@
 #include <panel-applet.h>
 #include <dbus/dbus-glib.h>
 
-/* AW: I pulled this code off the interwebs. It makes background 
- * transparency actually work
- */
-static void change_bg(
-    PanelApplet *applet, PanelAppletBackgroundType type,
-    GdkColor *color, GdkPixmap *pixmap)
-{
-    GtkRcStyle *rc_style;
-    GtkStyle *style;
-
-    /* reset style */
-    gtk_widget_set_style(GTK_WIDGET(applet), NULL);
-    rc_style = gtk_rc_style_new();
-    gtk_widget_modify_style(GTK_WIDGET(applet), rc_style);
-    gtk_rc_style_unref(rc_style);
-
-    switch(type) {
-    case PANEL_NO_BACKGROUND:
-        break;
-    case PANEL_COLOR_BACKGROUND:
-        gtk_widget_modify_bg(GTK_WIDGET(applet), GTK_STATE_NORMAL, color);
-        break;
-    case PANEL_PIXMAP_BACKGROUND:
-        style = gtk_style_copy(GTK_WIDGET(applet)->style);
-
-        if(style->bg_pixmap[GTK_STATE_NORMAL])
-            g_object_unref(style->bg_pixmap[GTK_STATE_NORMAL]);
-
-        style->bg_pixmap[GTK_STATE_NORMAL] = g_object_ref(pixmap);
-        gtk_widget_set_style(GTK_WIDGET(applet), style);
-        g_object_unref(style);
-        break;
-    }
-}
-
 static void signal_handler(DBusGProxy *obj, const char *msg, GtkWidget *widget)
 {
     gtk_label_set_markup(GTK_LABEL(widget), msg);
@@ -82,6 +47,8 @@ static gboolean xmonad_log_applet_fill(PanelApplet *applet)
         PANEL_APPLET_EXPAND_MAJOR |
         PANEL_APPLET_EXPAND_MINOR |
         PANEL_APPLET_HAS_HANDLE);
+
+    panel_applet_set_background_widget(applet, GTK_WIDGET(applet));
 
     GtkWidget *label = gtk_label_new("Waiting for Xmonad...");
     gtk_label_set_ellipsize(label, PANGO_ELLIPSIZE_END);
