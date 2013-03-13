204a5bbefed8139e7f6e2cb982f743797a470cbf
Ziga Ham <ziga@hamsworld.net>
2013-03-13 01:42:52 +0100

    Also send window id-s over DBus

--- xmonad.hs
+++ xmonad.hs
@@ -3,30 +3,30 @@
 import XMonad
 import XMonad.Config.Gnome
 import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.UrgencyHook
+import XMonad.Util.NamedWindows
+import XMonad.Util.WorkspaceCompare
 
+import Codec.Binary.UTF8.String (encodeString)
+import Data.List (intersperse)
+import Data.Maybe (isJust, catMaybes)
+
+import qualified Codec.Binary.UTF8.String as UTF8
 import qualified DBus as D
 import qualified DBus.Client as D
-import qualified Codec.Binary.UTF8.String as UTF8
+import qualified XMonad.StackSet as S
 
 main :: IO ()
 main = do
-    dbus <- D.connectSession
-    getWellKnownName dbus
-    xmonad $ gnomeConfig
-         { logHook = dynamicLogWithPP (prettyPrinter dbus)
-         }
+  dbus <- D.connectSession
+  getWellKnownName dbus
+  xmonad $ gnomeConfig {
+    logHook = iconsAppletLog dbus
+  }
 
-prettyPrinter :: D.Client -> PP
-prettyPrinter dbus = defaultPP
-    { ppOutput   = dbusOutput dbus
-    , ppTitle    = pangoSanitize
-    , ppCurrent  = pangoColor "green" . wrap "[" "]" . pangoSanitize
-    , ppVisible  = pangoColor "yellow" . wrap "(" ")" . pangoSanitize
-    , ppHidden   = const ""
-    , ppUrgent   = pangoColor "red"
-    , ppLayout   = const ""
-    , ppSep      = " "
-    }
+--
+-- dbus
+--
 
 getWellKnownName :: D.Client -> IO ()
 getWellKnownName dbus = do
@@ -36,22 +36,78 @@ getWellKnownName dbus = do
   
 dbusOutput :: D.Client -> String -> IO ()
 dbusOutput dbus str = do
-    let signal = (D.signal "/org/xmonad/Log" "org.xmonad.Log" "Update") {
-            D.signalBody = [D.toVariant ("<b>" ++ (UTF8.decodeString str) ++ "</b>")]
-        }
-    D.emit dbus signal
+  let signal = (D.signal "/org/xmonad/Log" "org.xmonad.Log" "Update") {
+      D.signalBody = [D.toVariant ((UTF8.decodeString str))]
+    }
+  D.emit dbus signal
 
-pangoColor :: String -> String -> String
-pangoColor fg = wrap left right
-  where
-    left  = "<span foreground=\"" ++ fg ++ "\">"
-    right = "</span>"
+--
+-- iconsAppletLog based on dynamicLogWithPP
+-- (in addition to workspace numbers, it also outputs window id-s)
+--
 
-pangoSanitize :: String -> String
-pangoSanitize = foldr sanitize ""
-  where
-    sanitize '>'  xs = "&gt;" ++ xs
-    sanitize '<'  xs = "&lt;" ++ xs
-    sanitize '\"' xs = "&quot;" ++ xs
-    sanitize '&'  xs = "&amp;" ++ xs
-    sanitize x    xs = x:xs
+iconsAppletLog :: D.Client -> X ()
+iconsAppletLog dbus = iconsAppletLogString >>= io . (dbusOutput dbus)
+
+iconsAppletLogString :: X String
+iconsAppletLogString = do
+
+    winset <- gets windowset
+    urgents <- readUrgents
+    sort' <- ppSort iconsAppletPP
+
+    -- layout description
+    let ld = description . S.layout . S.workspace . S.current $ winset
+
+    -- workspace list
+    let ws = iconsAppletWindowSet sort' urgents winset
+
+    -- window title
+    wt <- maybe (return "") (fmap show . getName) . S.peek $ winset
+
+    -- run extra loggers, ignoring any that generate errors.
+    extras <- mapM (flip catchX (return Nothing)) $ ppExtras iconsAppletPP
+
+    return $ encodeString . sepBy (ppSep iconsAppletPP) . ppOrder iconsAppletPP $
+                        [ ws
+                        , ppLayout iconsAppletPP ld
+                        , ppTitle iconsAppletPP wt
+                        ]
+                        ++ catMaybes extras
+
+iconsAppletWindowSet :: WorkspaceSort -> [Window] -> WindowSet -> String
+iconsAppletWindowSet sort' urgents s = sepBy ";" . map fmt . sort' $
+            map S.workspace (S.current s : S.visible s) ++ S.hidden s
+   where this     = S.currentTag s
+         visibles = map (S.tag . S.workspace) (S.visible s)
+
+         fmt w = printer iconsAppletPP (iconsAppletFormatWorkspace w)
+          where printer | any (\x -> maybe False (== S.tag w) (S.findTag x s)) urgents  = ppUrgent
+                        | S.tag w == this                                               = ppCurrent
+                        | S.tag w `elem` visibles                                       = ppVisible
+                        | isJust (S.stack w)                                            = ppHidden
+                        | otherwise                                                     = ppHiddenNoWindows
+
+iconsAppletFormatWorkspace :: S.Workspace WorkspaceId (Layout Window) Window -> String
+iconsAppletFormatWorkspace w = (S.tag w) ++ ":" ++ (iconsAppletFormatStack . S.stack $ w)
+
+iconsAppletFormatStack :: Maybe (S.Stack Window) -> String
+iconsAppletFormatStack s = case s of
+  Just value -> sepBy "," . map show $ (reverse (S.up value) ++ [S.focus value] ++ S.down value)
+  Nothing -> ""
+
+iconsAppletPP :: PP
+iconsAppletPP = defaultPP {
+    ppTitle    = const ""
+  , ppCurrent  = id
+  , ppVisible  = id
+  , ppHidden   = id
+  , ppUrgent   = id
+  , ppLayout   = const ""
+  , ppSep      = ""
+  }
+
+sepBy :: String   -- ^ separator
+      -> [String] -- ^ fields to output
+      -> String
+sepBy sep = concat . intersperse sep . filter (not . null)
